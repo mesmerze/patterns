ce089da23bf6a6e29396fc94500a4df9fbb6b85d
mesmerize <aichernovmail@gmail.com>
2021-09-14 20:37:01 +0200

    base

---
+++ spec/anything_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_hepler'
+require 'pry'
+
+require 'car_production_line'
+require 'chassis_state'
+require 'engine_state'
+require 'hood_state'
+require 'wheels_state'
+require 'illigal_jump_error'
+
+
+describe CarProductionLine do
+  let(:line) { CarProductionLine.new }
+
+  it '1st step - chassis' do
+    expect{ line.state }.to be(ChassisState)
+  end
+
+  it '2nd step - enging' do
+    line.install :engine
+
+    expect{ line.state }.to be(EngineState)
+  end
+
+  it '3rd step - hood' do
+    line.install :engine
+    line.install :hood
+
+    expect{ line.state }.to be(HoodState)
+  end
+
+  it '3rd step - hood' do
+    line.install :engine
+
+    expect{ line.install :wheels }.to raise_error(IlligalJumpError)
+  end
+end
