fba4a4d1a00a809df7ac4df1cc5e89b134014798
mesmerize <aichernovmail@gmail.com>
2021-09-14 20:37:01 +0200

    base

---
+++ spec/anything_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_hepler'
+require 'pry'
+
+require 'user'
+require 'manager'
+require 'payment_service_factory'
+require 'exceptions/forbidden'
+
+describe PaymentServiceFactory do
+  let(:user) { User.new('John Doe') }
+  let(:manager) { Manager.new('Boss') }
+
+  it 'allows manager to perform payments' do
+    expect(described_class.for(manager).pay(user, 500)).not_to raise_error
+  end
+
+  it 'forbids user toperform payments' do
+    expect(described_class.for(user).pay(manager, 500)).to raise_error(Forbidden)
+  end
+end
